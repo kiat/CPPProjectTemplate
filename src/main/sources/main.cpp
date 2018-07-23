483331d9668e52f3a6cb885856077081c7725b06
Kia Teymourian <kiat@bu.edu>
2018-07-22 22:08:38 -0400

    minor changes

--- src/main/sources/main.cpp
+++ src/main/sources/main.cpp
@@ -13,57 +13,65 @@ using namespace std;
 
 int main(int argc, char **argv) {
 
-
 	// Create 3 Objects
 	// No deletes and no new operators are used.
 
 	// This is my Auto.
-	auto  myAuto  =  std::make_shared <SedanCar>();
+	auto myAuto = std::make_shared<SedanCar>();
 	myAuto->setNoOfPassengers(4);
-	myAuto->setTailLoad(double (101.5));
+	myAuto->setTailLoad(double(101.5));
 
-	std::cout << "Total weight of my Sedan Car is: ";
-	std::cout <<  myAuto->getTotalWeight() << std::endl;
+	std::cout
+			<< "Total weight of my Sedan Car is: "
+					+ std::to_string(myAuto->getTotalWeight()) << std::endl;
+	std::cout << "No of References : " + std::to_string(myAuto.use_count())
+			<< std::endl;
+	std::cout << "=======   Separator  ======== " << std::endl;
 
-
-	std::cout << "=======   Separator  ======== "<< std::endl;
-
-    // And this is my truck.
-	auto  myTruck  =  std::make_shared <Truck>();
+	// And this is my truck.
+	auto myTruck = std::make_shared<Truck>();
 	myTruck->setNoOfPassengers(2);
-	myTruck->setTailLoad(double (500.50));
+	myTruck->setTailLoad(double(500.50));
 
-	std::cout << "Total weight of my Sedan Car is: ";
-	std::cout <<  myTruck->getTotalWeight() << std::endl;
+	std::cout
+			<< "Total weight of my Truck Car is: "
+					+ std::to_string(myTruck->getTotalWeight()) << std::endl;
 
-	std::cout << "=======   Separator  ======== "<< std::endl;
+	std::cout << "No of References : " + std::to_string(myTruck.use_count())
+			<< std::endl;
+
+	std::cout << "=======   Separator  ======== " << std::endl;
 
 	// Now let us make a vector of my cars.
 	std::vector<AutoPtr> cars;
 	cars.push_back(myAuto);
 	cars.push_back(myTruck);
 
-	double totalWeight=0;
-	for (AutoPtr c: cars) {
-		totalWeight+=c->getTotalWeight();
+	double totalWeight = 0;
 
+	for (AutoPtr c : cars) {
+		totalWeight += c->getTotalWeight();
 	}
 
 	std::cout << "Total weight of my cars are: ";
-			std::cout << 	+  totalWeight << std::endl;
+	std::cout << +totalWeight << std::endl;
 
-     std::cout << "=======   Separator  ======== "<< std::endl;
+	std::cout << "=======   Separator  ======== " << std::endl;
+
+	std::cout
+			<< "Now Objects have no references and are removed from memory automatically."
+			<< std::endl;
+
+	std::cout << "=======   Separator  ======== " << std::endl;
+
+	std::cout << "No of References : " + std::to_string(myAuto.use_count())
+			<< std::endl;
+	std::cout << "No of References : " + std::to_string(myTruck.use_count())
+			<< std::endl;
 
 
-     std::cout << "Now Objects have no references and are removed from memory automatically."<< std::endl;
-
-     std::cout << "=======   Separator  ======== "<< std::endl;
-
-
-    // No need to do the delete operations
+	// No need to do the delete operations
 	// Objects will be reference counted.
-
-
 
 // Running Valgrind as test
 
@@ -79,7 +87,8 @@ int main(int argc, char **argv) {
 //     ==4516== ERROR SUMMARY: 0 errors from 0 contexts (suppressed: 0 from 0)
 
 
+	std::cout << "=======   Separator  ======== " << std::endl;
 
-    return 0;
+	return 0;
 }
 
